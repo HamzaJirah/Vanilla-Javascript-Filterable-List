deef8abbc69331a5026f73968cb28a555c799966
HamzaJirah <hamzajirah@gmail.com>
2017-12-24 21:30:05 +0300

    get input element

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Get input element
+let filterInput = document.getElementById('filterInput')
+// Add event listener
+filterInput.addEventListener('keyup', filterNames);
 
+function filterNames(){
+  // Get value of input
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+  
+}
