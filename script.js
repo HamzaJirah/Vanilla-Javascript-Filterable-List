5a754d7be72e4ed6dc6e8c1cf586113ecfeee6a3
HamzaJirah <hamzajirah@gmail.com>
2017-12-24 21:36:01 +0300

    loop through lis

--- script.js
+++ script.js
@@ -6,5 +6,14 @@ filterInput.addEventListener('keyup', filterNames);
 function filterNames(){
   // Get value of input
   let filterValue = document.getElementById('filterInput').value.toUpperCase();
-  
+
+  // Get names ul
+  let ul = document.getElementById('names');
+  // Get lis from ul
+  let li = ul.querySelectorAll('li.collection-item');
+
+  // loop through colection-item lis
+  for(let i = 0; i< li.length; i++){
+    let a = li[i].getElementsByTagName('a')[0];
+  }
 }
